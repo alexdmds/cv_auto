4c6cf5103e79ccf2b81a16066412f281119665bd
alexdmds <38859202+alexdmds@users.noreply.github.com>
2025-02-26 17:19:44 +0100

    premier clean

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 from google.cloud import secretmanager
 
-
 import logging
 
 def configure_logging():
@@ -25,41 +24,28 @@ def configure_logging():
 
 class BaseConfig:
     # Valeurs communes à tous les environnements
-    LOCAL_PROMPT_PATH = Path(__file__).parent / "cv_automation/prompts"
     TEMP_PATH = Path("/tmp")  # Chemin temporaire
-    LOCAL_BASE_PATH = Path("data_local")  # Données locales par défaut
-    MOCK_OPENAI = False
     CHECK_AUTH = True
+    BUCKET_NAME = "cv-generator-447314.firebasestorage.app"
 
-    # Placeholder pour les variables spécifiques
     ENV = None
-    BUCKET_NAME = None
     OPENAI_API_KEY = None
-
-class LocalConfig(BaseConfig):
-    ENV = "local"
-    BUCKET_NAME = None  # Pas de bucket en local
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    CHECK_AUTH = False
 
 class DevConfig(BaseConfig):
     ENV = "dev"
-    BUCKET_NAME = "cv-generator-447314.firebasestorage.app"
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    MOCK_OPENAI = True
     CHECK_AUTH = False
 
 class ProdConfig(BaseConfig):
     ENV = "prod"
-    BUCKET_NAME = "cv-generator-447314.firebasestorage.app"
+    MOCK_OPENAI = False
     secret_manager_client = secretmanager.SecretManagerServiceClient()
     response = secret_manager_client.access_secret_version(request={"name": "projects/177360827241/secrets/OPENAI_API_KEY/versions/1"})
     OPENAI_API_KEY = response.payload.data.decode("UTF-8")
 
 def load_config():
-    env = os.getenv("ENV", "local")
-    if env == "dev":
-        return DevConfig()
-    elif env == "prod":
+    env = os.getenv("ENV", "dev")
+    if env == "prod":
         return ProdConfig()
     else:
-        return LocalConfig()
+        return DevConfig()
